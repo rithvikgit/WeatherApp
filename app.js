9df7adf38acd5355433b1b7ead791dd5b0adfdc4
rithvikgit <rithvik.r.poojary98@gamil.com>
2021-02-24 20:11:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/",function(req,res){
+
+  const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=75a0836e36af5cb79edd3eca6a590f7a"
+
+  https.get(url,function(response){
+    console.log(response.statuscode);
+
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data)
+      console.log(weatherData);
+    })
+  })
+  res.send("server is up and running");
+});
+
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000.");
+});
